add26013ff01e3d548885817a01563d06467be56
Jonnalagadda Lakshmi Prasanna <142923910+jlakshmiprasanna@users.noreply.github.com>
2024-10-21 21:10:00 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,19 @@
+// (Question-1)
+let str="Hello World!";
+console.log(str.replace("World!","My Dear Friend.."));
+
+// (Question-2)
+let a="Hungry";
+console.log(a.repeat(10));
+
+// (Question-3)
+let sentence="The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+// (Question-4)
+let message="Hello, world!";
+console.log(message.startsWith("hi"));
+
+// (Question-5)
+let fileName="document.pdf";
+console.log(fileName.endsWith(".pdf"))
